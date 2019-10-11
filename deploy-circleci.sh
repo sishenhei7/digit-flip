a799d124480c1f992cb8f0a79579eebf3829faa9
sishenhei7 <982505136@qq.com>
2019-10-11 08:22:09 +0800

    chore: test deploy

--- deploy-circleci.sh
+++ deploy-circleci.sh
@@ -1,5 +1,7 @@
 git pull
 yarn build:doc
+cd docs
+git init
 git add .
 git commit -m 'deploy: build docs'
-git push origin master
+git push git@github.com:sishenhei7/digit-flip.git  master:gh-pages
