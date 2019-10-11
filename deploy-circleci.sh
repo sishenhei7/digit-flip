2f93b08a478840a50bc1d6a5c004d50ff5050194
sishenhei7 <982505136@qq.com>
2019-10-11 10:48:48 +0800

    feat: add ci skip to commit msg

--- deploy-circleci.sh
+++ deploy-circleci.sh
@@ -5,6 +5,6 @@ git init
 git config --global user.email "you@example.com"
 git config --global user.name "sishenhei7"
 git add .
-git commit -m 'deploy: build docs'
+git commit -m 'deploy: build docs[ci skip]'
 git push -f git@github.com:sishenhei7/digit-flip.git  master:gh-pages
 cd -
